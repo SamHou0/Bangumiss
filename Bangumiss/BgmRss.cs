0a5a2019130f3a0a6d74df91724fc675ef4e4b3b
Sam Hou <sam@samhou.top>
2026-04-28 22:42:22 +0800

    feat: Add basic rss extraction

---
+++ Bangumiss/BgmRss.cs
@@ -0,0 +1,27 @@
+﻿using System.Xml;
+using System.ServiceModel.Syndication;
+namespace Bangumiss;
+
+public class BgmRss
+{
+    public required Uri BgmUri { get; set; }
+
+    public List<string> GetRssItems()
+    {
+        List<string> result = new();
+        using (XmlReader reader = XmlReader.Create(BgmUri.ToString()))
+        {
+            SyndicationFeed feed = SyndicationFeed.Load(reader);
+            Console.WriteLine("Reading feed from " + BgmUri.ToString());
+            Console.WriteLine(feed.Title.Text);
+            foreach (var item in feed.Items)
+            {
+                result.Add("Title:"+item.Title.Text+
+                           Environment.NewLine
+                +"link:"+ item.Links[0].Uri.AbsoluteUri);
+            }
+        }
+
+        return result;
+    }
+}
---
+++ Bangumiss/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Bangumiss;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        BgmRss bgmRss = new()
+        {
+            BgmUri = new Uri("https://bgm.tv/feed/user/samhou/timeline")
+        };
+        foreach (var item in bgmRss.GetRssItems())
+        {
+            Console.WriteLine(item);
+        }
+    }
+}
